ecce8bd9ee3b49e69be16a058a1d639f08ee9640
soraa14 <azizrizki1432002@gmail.com>
2021-05-30 10:40:35 -0400

    update

--- nmap_simplif1er.py
+++ nmap_simplif1er.py
@@ -3,48 +3,48 @@
 import os
 import time
 import argparse
+from colorama import Fore, Style
 
 # Initiate the parser
 parser = argparse.ArgumentParser()
 
 # Add arguments
-parser.add_argument("-i", "--ip", help="Target IP", required=True, dest="target_ip")
+parser.add_argument("--i", help="Target IP", dest="target_ip", required=True)
+parser.add_argument("--scsv", help="-sC -sV", nargs="?", const="-sC -sV")
+parser.add_argument("--ap", help="-p-", nargs="?", const="-p-")
+parser.add_argument("--all", help="-sC -sV -p-", nargs="?", const="-sC -sV -p-")
+
 
 # Read arguments from the command line
 args = parser.parse_args()
 
-# Target
+# Targets
 target = args.target_ip
 
-# NMAP
-nmap = "nmap"
-nmap_sc_sv = "-sC -sV"
-nmap_all_ports = "-p-"
+if args.target_ip:
+    normal_command = "nmap" + " " + target
+    print(Fore.GREEN + "Running... " + normal_command)
+    print(Style.RESET_ALL)
+    time.sleep(0.5)
+    os.system(normal_command)
 
+if args.scsv:
+    scsv_command = "nmap" + " " + args.scsv + " " + target
+    print(Fore.GREEN + "Running... " + scsv_command)
+    print(Style.RESET_ALL)
+    time.sleep(0.5)
+    os.system(scsv_command)
 
+if args.ap:
+    ap_command = "nmap" + " " + args.ap + " " + target
+    print(Fore.GREEN + "Running... " + ap_command)
+    print(Style.RESET_ALL)
+    time.sleep(0.5)
+    os.system(ap_command)
 
-print("\n");
-print("##########");
-print("\n");
-
-
-
-
-# # NIKTO
-# nikto_command = "nikto -host http://" + target + "/"
-# print("Running Nikto...");
-# print("* " + nikto_command);
-# time.sleep(0.5);
-# os.system(nikto_command);
-
-# print("\n");
-# print("##########");
-# print("\n");
-
-# # Gobuster
-# gobuster_command = "gobuster dir -u http://" + target + "/ -w /usr/share/seclists/Discovery/Web-Content/big.txt -x php,txt --random-agent"
-# print("Running Gobuster (with big.txt | php,txt extensions | random agent)...")
-# print("* " + gobuster_command);
-# time.sleep(0.5);
-# os.system(gobuster_command)
-
+if args.all:
+    all_command = "nmap" + " " + args.all + " " + target 
+    print(Fore.GREEN + "Running... " + all_command)  
+    print(Style.RESET_ALL)
+    time.sleep(0.5)
+    os.system(all_command)  
